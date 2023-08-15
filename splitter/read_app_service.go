e9910bddf44290f95ae424a5151d0a7d6bee6167
zhuweiyou <i@zhuweiyou.com>
2023-08-15 11:03:14 +0800

    整理

---
+++ splitter/read_app_service.go
@@ -0,0 +1,18 @@
+package splitter
+
+import (
+	"fmt"
+	"github.com/tidwall/gjson"
+	"os"
+	"path"
+)
+
+func ReadAppService(from string) (gjson.Result, error) {
+	fmt.Println("read app-service.js")
+	bytes, err := os.ReadFile(path.Join(from, "app-service.js"))
+	if err != nil {
+		return gjson.Result{}, fmt.Errorf("read app-service.js err: %v", err)
+	}
+
+	return gjson.ParseBytes(bytes), err
+}
